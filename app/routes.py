3090b16f58efc5dba80baa7b5bed407285d4e7b3
Oniel <51058739+0niel@users.noreply.github.com>
2021-06-14 13:04:41 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,22 +4,26 @@ from pymongo import ReturnDocument
 from . import sockets, questions_collection, chat_collection
 from .answers import AnswersDB
 
+
 @sockets.on('chat')
 def add_chat_message(data):
     room = data['room']
     data['message']['user'] = Markup.escape(data['message']['user'])
     data['message']['user_info'] = data['message']['user_info']
     data['message']['text'] = Markup.escape(data['message']['text'])
-    
+
     if len(data['message']['text']) > 0 and len(data['message']['text']) < 800:
-        chat_collection.find_one_and_update({'room': room}, {'$push': {'messages': data['message']}}, {"_id": 0}, upsert=True)
+        chat_collection.find_one_and_update(
+            {'room': room}, {'$push': {'messages': data['message']}}, {"_id": 0}, upsert=True)
         sockets.emit('add_chat_messages', [data['message']], to=room)
-    
+
+
 @sockets.on('get_chat')
 def get_chat_messages(room):
     messages = chat_collection.find_one({'room': room}, {"_id": 0})
     if messages is not None:
         sockets.emit('add_chat_messages', messages['messages'], to=room)
+
 
 @sockets.on('view_question')
 def ans_view(message):
@@ -33,17 +37,21 @@ def ans_view(message):
         for question in questions:
             data = AnswersDB.add_new_viewer(question, user_info)
             result['data'].append(data)
-            
+
         sockets.emit('update_viewers', result, to=room)
-        
+
+
 @sockets.on('add_answer')
 def ans_view(data):
-    result = AnswersDB.add_user_answer(data['question'], data['answer'], data['user_info'], data['question_type'])
+    result = AnswersDB.add_user_answer(
+        data['question'], data['answer'], data['user_info'], data['question_type'])
     sockets.emit('update_answers', result, to=data['room'])
+
 
 @sockets.on('join')
 def on_join(room):
     join_room(room)
+
 
 @sockets.on('leave')
 def on_leave(room):
