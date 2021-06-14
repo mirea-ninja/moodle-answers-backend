217dd6b38099704a9b8ca86fae5ccb259132e1b5
Oniel <51058739+0niel@users.noreply.github.com>
2021-06-14 12:46:20 +0300

    Add SSL context

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import sockets, app
 
 
 if __name__ == "__main__":
-    sockets.run(app)
+    sockets.run(app, host='0.0.0.0', ssl_context=(
+        '/var/discourse/shared/standalone/ssl/mirea.ninja.cer', '/var/discourse/shared/standalone/ssl/mirea.ninja.key'))
