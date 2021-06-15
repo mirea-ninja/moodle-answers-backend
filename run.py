2a98909c19ac9021cb827aac97ad7508a01fb7d6
Oniel <51058739+0niel@users.noreply.github.com>
2021-06-15 16:35:00 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from app import sockets, app
 
 
 if __name__ == "__main__":
-    # sockets.run(app, host='0.0.0.0', ssl_context=(
-    #     '/var/discourse/shared/standalone/ssl/mirea.ninja.cer', '/var/discourse/shared/standalone/ssl/mirea.ninja.key'))
+    # sockets.run(app, debug=False, host='0.0.0.0', certfile='path_here', keyfile='path_here')
     sockets.run(app, host='0.0.0.0')
