fd90b3a07bee14e75cd455bb01128516f5fbd8ba
Oniel <51058739+0niel@users.noreply.github.com>
2021-06-15 12:04:52 +0300

    Add threading async mode

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,6 @@ app = Flask(__name__)
 app.debug = True
 app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
 
-sockets = SocketIO(app, cors_allowed_origins="*")
+sockets = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 from . import answers, routes
